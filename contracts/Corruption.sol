a797d1aef68f467643ffa13c972274de6b334dd2
Steven Matthiesen <steven.matthiesen@gmail.com>
2019-10-26 16:17:59 -0700

    smart contract launch complete

--- contracts/Corruption.sol
+++ contracts/Corruption.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.3;
+import './cDao.sol';
 
 contract Corruption {
     
@@ -11,7 +12,17 @@ contract Corruption {
         string hexForLicense;
     }
 
+    //Amount of tokens in your contract
+    mapping(address => uint256) public tokens;
+
+    //How much can this contract send out --- not needed I think.
+    //mapping(address => mapping(address => uint256)) public allowance;
+
+    //Map the contractors info to their address - so it can be easily referenced
     mapping(address => contractorInfo) public contractor;
+
+    //This is for debugging /tracking successful builds
+    address[] allContracts;
 
     //This will be the address of the us
     address ourWallet;
@@ -30,6 +41,12 @@ contract Corruption {
 
     //This is the amount of money being issues
     uint fundAmount;
+
+    //Amount of tokens created
+    uint totalTokens;
+
+    //Current amount of tokens left
+    uint currentTokens;
 
     //This person
     address fundReceiver;
@@ -59,12 +76,43 @@ contract Corruption {
     }
 
     function doCreateCorruptionContract(string memory _fundName, address _debtIssuer, address _operatorOwner, uint _fundAmount, address[] memory _subContractors) public {
-        fundId = address(this);
-        fundName = _fundName;
-        debtIssuer = _debtIssuer;
-        operatorOwner = _operatorOwner;
-        fundAmount = _fundAmount;
-        subContractors = _subContractors;
-        creationDate = now;
+        cDao c = new cDao(_fundName,  _debtIssuer, _operatorOwner, _fundAmount, _subContractors);
+        allContracts.push(address(c));
+    }
+
+    function getAllContracts() public view returns (address[] memory){
+        return allContracts;
+    }
+
+    function getTotalTokens(address _contract) public view returns (uint) {
+        return cDao(_contract).getTotalTokens();
+    }
+
+    function getDaoInfo(address _contract) public view returns (address, string memory, uint, uint, address, address) {
+        return cDao(_contract).getDaoInfo();
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        //require(balanceOf[msg.sender] >= _value);
+        //balanceOf[msg.sender] -= _value;
+        //balanceOf[_to] += _value;
+        //Transfer(msg.sender, _to, _value);
+        //return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        //allowance[msg.sender][_spender] = _value;
+        //Approval(msg.sender, _spender, _value);
+        //return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        //require(_value <= balanceOf[_from]);
+        //require(_value <= allowance[_from][msg.sender]);
+        //balanceOf[_from] -= _value;
+        //balanceOf[_to] += _value;
+        //allowance[_from][msg.sender] -= _value;
+        //Transfer(_from, _to, _value);
+        //return true;
     }
 }
